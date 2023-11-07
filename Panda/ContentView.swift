963726aa63610aad6bc4d32570a30275791b5bdc
EddieZhan <nianyi234@gmail.com>
2023-11-07 20:08:49 +0800

    Initial Commit

---
+++ Panda/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Panda
+//
+//  Created by 战璐 on 2023/11/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Panda/PandaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PandaApp.swift
+//  Panda
+//
+//  Created by 战璐 on 2023/11/7.
+//
+
+import SwiftUI
+
+@main
+struct PandaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
