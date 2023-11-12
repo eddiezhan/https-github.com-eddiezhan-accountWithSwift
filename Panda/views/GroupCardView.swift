a776929fd6ea64a4e6d3422f2a231ec4ca0e090e
EddieZhan <nianyi234@gmail.com>
2023-11-12 23:26:36 +0800

    增加了费用的 添加和删除功能

---
+++ Panda/views/GroupCardView.swift
@@ -0,0 +1,18 @@
+//
+//  GroupCardView.swift
+//  Panda
+//
+//  Created by EddieZhan on 2023/11/12.
+//
+
+import SwiftUI
+
+struct GroupCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupCardView()
+}
